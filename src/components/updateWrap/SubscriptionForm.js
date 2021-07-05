5f902abada3e3c1e0e68f2765197a689143cdec7
Dip Vachhani <dv.dipvachhani@gmail.com>
2021-07-05 14:10:04 +0530

    | Update Subscription Section |

--- src/components/updateWrap/SubscriptionForm.js
+++ src/components/updateWrap/SubscriptionForm.js
@@ -4,18 +4,12 @@ const SubscriptionForm = props => {
   const { placeholder, label } = props
 
   return (
-    <div className="col-sm-5 col-12 right-conn">
-      <form action className="subscription_form">
-        <div className="form-group mb-0">
-          <input
-            type="text"
-            placeholder={placeholder}
-            className="form-control"
-          />
-          <button className="btn-main">{label}</button>
-        </div>
-      </form>
-    </div>
+    <form action className="subscription_form">
+      <div className="form-group mb-0">
+        <input type="text" placeholder={placeholder} className="form-control" />
+        <button className="btn-main">{label}</button>
+      </div>
+    </form>
   )
 }
 
--- src/container/learn/SubscribeWrapSection.js
+++ src/container/learn/SubscribeWrapSection.js
@@ -5,17 +5,21 @@ const SubscribeWrapSection = props => {
   const { title, description, inputPlaceholder, submitLabel } = props
 
   return (
-    <div className="subscribe-raw inner section ptb100">
-      <div className="subscribe-col section">
-        <div className="row align-items-center">
-          <div className="col-sm-7 col-12 left-conn">
-            <h2 dangerouslySetInnerHTML={{ __html: title }} />
-            <p dangerouslySetInnerHTML={{ __html: description }} />
+    <div className="container">
+      <div className="subscribe-raw inner section ptb100">
+        <div className="subscribe-col section">
+          <div className="row align-items-center">
+            <div className="col-sm-7 col-12 left-conn">
+              <h2 dangerouslySetInnerHTML={{ __html: title }} />
+              <p dangerouslySetInnerHTML={{ __html: description }} />
+            </div>
+            <div className="col-sm-5 col-12 right-conn">
+              <SubscriptionForm
+                placeholder={inputPlaceholder}
+                label={submitLabel}
+              />
+            </div>
           </div>
-          <SubscriptionForm
-            placeholder={inputPlaceholder}
-            label={submitLabel}
-          />
         </div>
       </div>
     </div>
