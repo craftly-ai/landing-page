ad429c61a433aa793168d0098d736743a50cf5be
Dip Vachhani <dv.dipvachhani@gmail.com>
2021-07-13 20:35:23 +0530

    | Build With Path Prefix |

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => (
         <div className="row">
           <div className="col-sm-3 col-xs-12 left-conn">
             <div className="logo">
-              <a href="/">
+              <a href="/craftly-landing/">
                 <img src={logo} className="img-responsive" alt="Craftly" />
               </a>
             </div>
@@ -19,10 +19,10 @@ const Footer = () => (
               <h5>Company</h5>
               <ul>
                 <li>
-                  <a href="/about/">About</a>
+                  <a href="/craftly-landing/about/">About</a>
                 </li>
                 <li>
-                  <a href="/#pricing">Pricing</a>
+                  <a href="/craftly-landing/#pricing">Pricing</a>
                 </li>
                 <li>
                   <a href="#header">Demo</a>
