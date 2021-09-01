c8ea4ec8caf2ce50575ffb4c0e38f09daf3f8c4b
Dip Vachhani <dv.dipvachhani@gmail.com>
2021-09-01 12:00:27 +0530

    | Update Request Access |

--- src/container/home/IntroSection.js
+++ src/container/home/IntroSection.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Typewriter from "../../components/Typewriter/Typewriter"
 import img from "../../images/banner-bg-image-min.png"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 const IntroSection = () => {
   const data = useStaticQuery(graphql`
@@ -44,26 +44,13 @@ const IntroSection = () => {
               dataRotate={typerText}
             />
             <p>{bannerShortDescription}</p>
-            <div className="request-access-raw section">
-              <div className="form-group">
-                <label htmlFor>{requestAccessLabel}</label>
-                <div className="prefinery-form-embed">
-                  <iframe
-                    title="email input"
-                    src="https://i.prefinery.com/projects/g72qxl5b/users/new?display=inline&amp;version=2"
-                    class="prefinery-iframe"
-                    allowtransparency="true"
-                    width="100%"
-                    scrolling="no"
-                    frameborder="0"
-                    style={{ height: "0px" }}
-                  ></iframe>
-                </div>
-              </div>
-              {/* <h6>
-                <span /> {freeTrialLabel}
-              </h6> */}
-            </div>
+            <Link
+              data-toggle="modal"
+              data-target="#join-waitlist"
+              className="btn-main"
+            >
+              {"Request Access"}
+            </Link>
           </div>
           <div className="col-sm-6 col-12 justify-content-center align-self-center right-conn">
             <div className="img-box" id="lottie">
